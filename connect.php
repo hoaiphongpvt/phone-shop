ef81040dadaf39db368a62f667f365a783ebd959
hoaiphongpvt <hoaiphongpvt@gmail.com>
2023-07-04 01:26:49 +0700

    update db

--- connect.php
+++ connect.php
@@ -6,10 +6,10 @@
 // $conn = mysqli_connect($host, $uname, $pwd, $db_name);
 // mysqli_set_charset($conn, 'UTF8');
 
-$host ="localhost";
-$uname = "root";
-$pwd = '';
-$db_name = "store";
+$host ="us-cdbr-east-06.cleardb.net";
+$uname = "baf581ab629dee";
+$pwd = '03fa0ace';
+$db_name = "heroku_1b2b1193fa8d0f4";
 $conn = mysqli_connect($host, $uname, $pwd, $db_name);
 mysqli_set_charset($conn, 'UTF8');
 
